e0c797fe44b5a144b446f2be1b03c76aaae51a5d
TanTanner <tantanner@TanTannerdeMacBook-Pro.local>
2016-04-30 14:47:47 +0800

    update view rspec

--- spec/features/support/env.rb
+++
@@ -1,5 +0,0 @@
-Capybara.register_driver :selenium do |app|
-  require 'selenium/webdriver'
-  Selenium::WebDriver::Firefox::Binary.path='C:\Users\tanner.tan\AppData\Local\Mozilla Firefox\firefox.exe'
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-end
